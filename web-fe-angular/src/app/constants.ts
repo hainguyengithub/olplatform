4a5542a42fee993df45468783935bd9dcb075856
Hai Nguyen <h_nguyen89212@fanshaweonline.ca>
2021-09-18 10:53:48 -0400

    constants for app

---
+++ web-fe-angular/src/app/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Environment-specific constants.
+ */
+export const SERVER_BASE_URL = 'http://localhost:8080';
+
+/**
+ * Environment-agnostic constants.
+ */
+export const API_PROGRAMS = 'api/programs';
