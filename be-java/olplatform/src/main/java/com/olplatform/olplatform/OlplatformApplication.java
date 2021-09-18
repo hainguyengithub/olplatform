dcbdd8e05307fa7ecb35c790c8e218c823fcbca6
Hai Nguyen <h_nguyen89212@fanshaweonline.ca>
2021-09-18 08:57:30 -0400

    clean slate web-fe-angular, be-java, be-aspnet

---
+++ be-java/olplatform/src/main/java/com/olplatform/olplatform/OlplatformApplication.java
@@ -0,0 +1,13 @@
+package com.olplatform.olplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlplatformApplication.class, args);
+	}
+
+}
