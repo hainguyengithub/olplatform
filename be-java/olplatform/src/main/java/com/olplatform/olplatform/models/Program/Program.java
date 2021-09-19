54e86a50ea5c3d224093791d6836b9d74cbae886
Hai Nguyen <h_nguyen89212@fanshaweonline.ca>
2021-09-19 19:40:42 -0400

    1-N with academic advisor

--- be-java/olplatform/src/main/java/com/olplatform/olplatform/models/Program/Program.java
+++ be-java/olplatform/src/main/java/com/olplatform/olplatform/models/Program/Program.java
@@ -1,9 +1,14 @@
 package com.olplatform.olplatform.models.Program;
 
+import com.olplatform.olplatform.models.AcademicAdvisor.AcademicAdvisor;
 import com.olplatform.olplatform.models.Administrable;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Program extends Administrable {
-  // @todo: program administrator
+  @ManyToOne
+  @JoinColumn(name = "id", nullable = false)
+  protected AcademicAdvisor academicAdvisor;
 }
