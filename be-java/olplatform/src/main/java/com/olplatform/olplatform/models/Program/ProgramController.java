4d022046b5726379a80baf2a93a3ac2ed1f66e43
Hai Nguyen <h_nguyen89212@fanshaweonline.ca>
2021-09-19 18:04:33 -0400

    deleteOne for Program model

--- be-java/olplatform/src/main/java/com/olplatform/olplatform/models/Program/ProgramController.java
+++ be-java/olplatform/src/main/java/com/olplatform/olplatform/models/Program/ProgramController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,5 +34,11 @@ public class ProgramController {
   public ResponseEntity<Program> addOne(@RequestBody Program program) {
     Program createdProgram = this.programRepository.save(program);
     return new ResponseEntity<Program>(createdProgram, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/api/programs/{programId}")
+  public ResponseEntity<Integer> deleteOne(@PathVariable long programId) {
+    int result = this.programRepository.deleteOne(programId);
+    return new ResponseEntity<Integer>(result, HttpStatus.OK);
   }
 }
--- be-java/olplatform/src/main/java/com/olplatform/olplatform/models/Program/ProgramRepository.java
+++ be-java/olplatform/src/main/java/com/olplatform/olplatform/models/Program/ProgramRepository.java
@@ -1,9 +1,18 @@
 package com.olplatform.olplatform.models.Program;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "programs", path = "programs")
-public interface ProgramRepository extends CrudRepository<Program, Long> {}
+public interface ProgramRepository extends CrudRepository<Program, Long> {
+  @Modifying
+  @Transactional
+  @Query("DELETE FROM Program WHERE id = ?1")
+  public int deleteOne(Long programId);
+}
