ae604f00bd02c80e3d4154b2e5cf0db87ee7318b
Hai Nguyen <hainguyen@Hais-MBP.hitronhub.home>
2021-09-21 00:13:29 -0400

    resolve warnings

--- be-java/olplatform/src/main/java/com/olplatform/olplatform/models/AcademicAdvisor/AcademicAdvisor.java
+++ be-java/olplatform/src/main/java/com/olplatform/olplatform/models/AcademicAdvisor/AcademicAdvisor.java
@@ -14,8 +14,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "academic_advisor")
 public class AcademicAdvisor extends Person {
--- be-java/olplatform/src/main/java/com/olplatform/olplatform/models/Student/Student.java
+++ be-java/olplatform/src/main/java/com/olplatform/olplatform/models/Student/Student.java
@@ -2,20 +2,19 @@ package com.olplatform.olplatform.models.Student;
 
 import com.olplatform.olplatform.models.Person;
 import com.olplatform.olplatform.models.Seat.Seat;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "student")
 public class Student extends Person {
@@ -35,8 +34,8 @@ public class Student extends Person {
 
   protected String phoneNumber;
 
-  @OneToMany(mappedBy = "student") /* student is an attribute in ClassRegistration. */
-  protected List<Seat> classRegistrations = new ArrayList<>();
+  @OneToMany(mappedBy = "student")/* student is an attribute in Seat. */
+  protected List<Seat> seats = new ArrayList<>();
 
   public Student() {
     super();
