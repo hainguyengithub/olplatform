737dce820213b95af49b21721d730289434ca6fa
Hai Nguyen <h_nguyen89212@fanshaweonline.ca>
2021-09-22 19:22:24 -0400

    import MaterialComponentModule into program

--- web-fe-angular/src/app/program/program.module.ts
+++ web-fe-angular/src/app/program/program.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProgramListWidgetComponent } from './program-list-widget/program-list-widget.component';
 import { ProgramLandingPageComponent } from './program-landing-page/program-landing-page.component';
-
-
+import { MaterialComponentsModule } from '../material-components/material-components.module';
 
 @NgModule({
-  declarations: [
-    ProgramListWidgetComponent,
-    ProgramLandingPageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ProgramListWidgetComponent, ProgramLandingPageComponent],
+  imports: [CommonModule, MaterialComponentsModule],
 })
-export class ProgramModule { }
+export class ProgramModule {}
