283c1e757c41c755466528f0280a3f634b476803
Hai Nguyen <h_nguyen89212@fanshaweonline.ca>
2021-09-22 19:19:11 -0400

    add /programs endpoint to routes

--- web-fe-angular/src/app/app-routing.module.ts
+++ web-fe-angular/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProgramListWidgetComponent } from './program/program-list-widget/program-list-widget.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'programs',
+    component: ProgramListWidgetComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
