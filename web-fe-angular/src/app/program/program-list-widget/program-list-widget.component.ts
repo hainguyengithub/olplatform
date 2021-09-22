9715e6c7e4bf69d3e3fcb36b1e375a6a18700a80
Hai Nguyen <h_nguyen89212@fanshaweonline.ca>
2021-09-22 19:22:47 -0400

    test out querying programs

--- web-fe-angular/src/app/program/program-list-widget/program-list-widget.component.ts
+++ web-fe-angular/src/app/program/program-list-widget/program-list-widget.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Program } from '../Program';
+import { ProgramService } from '../service/program.service';
 
 @Component({
   selector: 'app-program-list-widget',
   templateUrl: './program-list-widget.component.html',
-  styleUrls: ['./program-list-widget.component.scss']
+  styleUrls: ['./program-list-widget.component.scss'],
 })
 export class ProgramListWidgetComponent implements OnInit {
+  // @Input() public programs: Program[] | null = [];
+  public programs: Program[] | null = [];
 
-  constructor() { }
+  constructor(private programService: ProgramService) {}
 
   ngOnInit(): void {
+    this.programService.load().subscribe((payload) => {
+      console.log(payload);
+      this.programs = payload;
+    });
   }
-
 }
