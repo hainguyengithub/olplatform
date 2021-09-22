b729860e9e20a01c80ce49cf41eded4623cc9f68
Hai Nguyen <h_nguyen89212@fanshaweonline.ca>
2021-09-22 19:20:30 -0400

    import HttpClientModule and ProgramModule into app

--- web-fe-angular/src/app/app.module.ts
+++ web-fe-angular/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,6 +8,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 import { MaterialComponentsModule } from './material-components/material-components.module';
+import { ProgramModule } from './program/program.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -15,6 +17,9 @@ import { MaterialComponentsModule } from './material-components/material-compone
     BrowserAnimationsModule,
     AppRoutingModule,
     MaterialComponentsModule,
+    HttpClientModule /* Not HttpClient; Otherwise, JIT compiler fails. */,
+    //
+    ProgramModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
